cffe143ae3eb64b01160de2262d48896ecace2ce
Pedro Achcar <pedro.achcar@hotmail.com>
2021-10-21 20:31:18 -0300

    inicial templates - home and about

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'title': 'Post 1',
+        'author': 'Pedro Achcar',
+        'content': 'Primeiro post do blog!',
+        'date_posted': '21/10/2021'
+    },
+    {
+        'title': 'Post 2',
+        'author': 'Pedro Lucas',
+        'content': 'Segundo post do blog!',
+        'date_posted': '20/10/2021'
+    },
+    {
+        'title': 'Post 3',
+        'author': 'Nathalia Costa',
+        'content': 'Terceiro post do blog!',
+        'date_posted': '22/10/2021'
+    },
+    {
+        'title': 'Post 4',
+        'author': 'Teste',
+        'content': 'Quarto e último post o blog!',
+        'date_posted': '23/10/2021'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    post = {'posts': posts}
+    return render(request, 'blog/home.html', post)
 
 
 def about(request):
-    return HttpResponse('<h1>Sobre o Blog</h1>')
+    return render(request, 'blog/about.html', {'title': ''})
