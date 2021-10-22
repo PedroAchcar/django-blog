f8091395baefc9bc9e958a19984033350bbcf426
Pedro Achcar <pedro.achcar@hotmail.com>
2021-10-22 13:26:59 -0300

    first model-Post; register blog in admin

--- blog/views.py
+++ blog/views.py
@@ -1,35 +1,10 @@
 from django.shortcuts import render
 
-posts = [
-    {
-        'title': 'Post 1',
-        'author': 'Pedro Achcar',
-        'content': 'Primeiro post do blog!',
-        'date_posted': '21/10/2021'
-    },
-    {
-        'title': 'Post 2',
-        'author': 'Pedro Lucas',
-        'content': 'Segundo post do blog!',
-        'date_posted': '20/10/2021'
-    },
-    {
-        'title': 'Post 3',
-        'author': 'Nathalia Costa',
-        'content': 'Terceiro post do blog!',
-        'date_posted': '22/10/2021'
-    },
-    {
-        'title': 'Post 4',
-        'author': 'Teste',
-        'content': 'Quarto e último post o blog!',
-        'date_posted': '23/10/2021'
-    }
-]
+from .models import Post
 
 
 def home(request):
-    post = {'posts': posts}
+    post = {'posts': Post.objects.all().order_by('-date_posted')}
     return render(request, 'blog/home.html', post)
 
 
