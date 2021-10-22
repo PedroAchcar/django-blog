1fc4331e50e98a0ec6d8fa728e42541727563e84
Pedro Achcar <pedro.achcar@hotmail.com>
2021-10-22 16:13:11 -0300

    adding email form; learning crispy_forms

--- users/views.py
+++ users/views.py
@@ -1,15 +1,17 @@
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
+
+from .forms import UserRegisterForm
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Conta criada com usuário {username}!')
             return redirect('blog-home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form, 'title': 'Registre-se aqui'})
