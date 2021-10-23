f054dcabba43106d618a1336d0c6a844be22ead7
Pedro Achcar <pedro.achcar@hotmail.com>
2021-10-23 19:46:21 -0300

    user profile and profile picture

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=300)
+    image = models.ImageField(
+        default='profile_default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'Perfil do {self.user.username}'
