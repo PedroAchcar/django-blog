66b696c42c2214dd40508cae4951c4f8c8133b29
Pedro Achcar <pedro.achcar@hotmail.com>
2021-10-23 22:14:40 -0300

    resize image for 300px-300px; post image on post

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -10,3 +11,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'Perfil do {self.user.username}'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            img.thumbnail((300, 300))
+            img.save(self.image.path)
