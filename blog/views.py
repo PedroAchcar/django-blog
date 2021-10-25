de3abc023406875cd6c2d82d96386fc6a6b1c05f
Pedro Achcar <pedro.achcar@hotmail.com>
2021-10-24 23:30:00 -0300

    List and Detail Post views with Class; post name on home working as link

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
 
-def home(request):
-    post = {'posts': Post.objects.all().order_by('-date_posted')}
-    return render(request, 'blog/home.html', post)
+# def home(request):
+#     post = {'posts': Post.objects.all().order_by('-date_posted')}
+#     return render(request, 'blog/home.html', post)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
 
 
 def about(request):
